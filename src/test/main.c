45d582cd621c366f8dbf3b2f79a50fb331f54a9e
akkalvin39-sudo <akkalvin39@gmail.com>
2026-02-04 08:45:12 +0700

    build: add cppcheck rule to Makefile
    Add a rule to run cppcheck (static analyser) on the files of
    the project to find additional errors that the compiler doesn't
    catch.
    
    video: 9

---
+++ src/test/main.c
@@ -0,0 +1,11 @@
+// cppcheck-suppress unusedFunction
+int sum(int a, int b)
+{
+    return a + b;
+}
+int main(void)
+{
+ // cppcheck-suppress constVariable
+    int arr[5]={0};
+    return arr[1];
+}
