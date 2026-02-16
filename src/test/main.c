3ebbff417752910251d3cd5ea28d8f0effdbc05b
akkalvin39-sudo <akkalvin39@gmail.com>
2026-02-16 21:49:29 +0700

    ci: update docker + formatting adjustments

--- src/test/main.c
+++ src/test/main.c
@@ -1,11 +1,8 @@
 // cppcheck-suppress unusedFunction
-int sum(int a, int b)
-{
-    return a + b;
-}
+int sum(int a, int b) { return a + b; }
 int main(void)
 {
- // cppcheck-suppress constVariable
-    int arr[5]={0};
+    // cppcheck-suppress constVariable
+    int arr[5] = { 0 };
     return arr[2];
 }
